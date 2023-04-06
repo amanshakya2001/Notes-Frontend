f1e2c0f2f03575cd09a74f4065eb4f2040edd749
amanshakya2001 <amanshakya360@gmail.com>
2023-04-06 22:12:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
+import $ from 'jquery';
+
 
 function App() {
+  const [localData,setLocalData] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:3000')
+    .then(responce=>responce.json())
+    .then(data=>{
+      console.log(data.data)
+      setLocalData(data.data);
+    })
+    .catch(error=>{
+    })
+  }, []);
+
+  // Api call to delete element
+  const deleteElement = (key)=>{
+    fetch('http://localhost:3000/deleteNote',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({title:key})
+    })
+    .then(responce=>responce.json())
+    .then(data=>{
+      setLocalData(data.data);
+    })
+    .catch(error=>{
+    })
+  }
+
+  // Api call to add element
+  $('#dataForm').submit((event)=>{
+    event.preventDefault();
+    if($('#title').val() === "" || $('#data').val() === ""){
+      console.log("empty")
+    }
+    else{
+      let value = {
+        title:$('#title').val(),
+        data:$('#data').val()
+      }
+      fetch('http://localhost:3000/createNote',{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify(value)
+      })
+      .then(responce=>responce.json())
+      .then(data=>{
+        setLocalData(data.data);
+        $('#title').val("");
+        $('#data').val("");
+      })
+      .catch(error=>{
+      })
+    }
+    return false;
+  })
+
+  // Format dateTime
+  const formatDateTime = (element)=>{
+    let date = element.split('T')
+    return date[0]+" "+date[1].split('.')[0]
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="form-section">
+        <div className="container">
+          <form action="POST" className="w-75 mx-auto my-5" id="dataForm">
+            <div className="mb-3">
+              <label htmlFor="title" className="form-label">Title</label>
+              <input type="text" className="form-control" id="title" placeholder="Title" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="data" className="form-label">Data</label>
+              <input type="text" className="form-control" id="data" placeholder="Note" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </section>
+      <section className="Notes">
+        <div className="container">
+          {localData.length ? localData.map((element,index)=>{
+              return <div key={index} className="card">
+                  <div className="card-title py-2 px-4">{element.title}</div>
+                  <div className="card-body px-4">{element.data}</div>
+                  <div className="card-footer">{formatDateTime(element.datetime)} <button className="btn btn-danger rounded-circle p-2" onClick={()=>{deleteElement(element.title)}}>
+                    <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M12 38c0 2.21 1.79 4 4 4h16c2.21 0 4-1.79 4-4v-24h-24v24zm26-30h-7l-2-2h-10l-2 2h-7v4h28v-4z"/><path d="M0 0h48v48h-48z" fill="none"/>
+                    </svg>
+                  </button>
+                  </div>
+                </div>
+            }):<p className="text-center">No data found</p>}
+        </div>
+      </section>
+    </>
   );
 }
 
