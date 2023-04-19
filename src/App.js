92d44b8100c02cd870ddd7375d33527b7409dadc
Aman Shakya <aman.shakya@analyticsvidhya.com>
2023-04-19 15:48:01 +0530

    Revert code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { useState,useEffect } from "react";
 import $ from 'jquery';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
-const provider = new GoogleAuthProvider();
 
 
 function App() {
@@ -19,26 +16,6 @@ function App() {
     })
     .catch(error=>{
     })
-    const auth = getAuth();
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
   }, []);
 
   // Api call to delete element
